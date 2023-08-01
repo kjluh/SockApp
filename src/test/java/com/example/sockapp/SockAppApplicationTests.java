8a01fe277164309f3023bd7f0245d514457a1178
Anatoly <kjluh@mail.ru>
2023-08-01 22:10:58 +0300

    Initial commit

---
+++ src/test/java/com/example/sockapp/SockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
